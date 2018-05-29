7fb0b4550b441d2bcfaa92699e0dd1393df56dba
Vagelis Manavis <vagelis.manavis@gmail.com>
2018-05-29 23:08:32 +0300

    Connect with MySQL

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import pymysql.cursors
+
+connection = pymysql.connect('localhost', 'root', 'toor', 'cerberusDB')
+
+try:
+    #with connection.cursor() as cursor:
+    #    sql = "INSERT INTO `user` (`username`, `password`, `email`) VALUES (%s, %s, %s)"
+    #    cursor.execute(sql, ('evanmana', 'asdfasdf', 'vagelis.manavis@gmail.com'))
+
+    #connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM `user`"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        print(result)
+finally:
+    connection.close()
+
+
 print("Hello Cerberus :)")
