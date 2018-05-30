a5202e2be49757f6bca426023aeb1a6ba88a9117
Vagelis Manavis <vagelis.manavis@gmail.com>
2018-05-30 22:27:50 +0300

    Add services into MySQL

--- addNewServiceForm.py
+++ addNewServiceForm.py
@@ -1,14 +1,26 @@
 from tkinter import *
+import connectDB
+
 
 def show_entry_fields():
-   print("First Name: %s\nLast Name: %s" % (eService.get(), eEmail.get()))
+   print("%s\t%s\t%s\t%s" % (eService.get(), eEmail.get(), eUsername.get(), ePassword.get()))
+
+def insertService():
+   print("Connecting to MySQL...")
+   connection = connectDB.connect()
+   print("Connected to MySQL...")
+   try:
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO `account` (`nameService`, `email`, `username`,  `password`) VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql, (eService.get(), eEmail.get(), eUsername.get(), ePassword.get()))
+    connection.commit()
+   finally:
+    connection.close()
+
 
 master = Tk()
 master.title('Εισαγωγή Νέας Υπηρεσίας')
-#master.geometry("330x300") #You want the size of the app to be 500x500
-master.resizable(0, 0) #Don't allow resizing in the x or y direction
-
-
+master.resizable(0, 0)
 
 Label(master, text="Υπηρεσία:").grid(row=0, sticky="W", padx=5, pady=5)
 Label(master, text="Email:").grid(row=1, sticky="W", padx=5, pady=5)
@@ -20,12 +32,12 @@ eEmail = Entry(master, width=35)
 eUsername = Entry(master, width=35)
 ePassword = Entry(master, width=35)
 
-eService.grid(row=0, column=1, pady=5)
-eEmail.grid(row=1, column=1, pady=5)
-eUsername.grid(row=2, column=1, pady=5)
-ePassword.grid(row=3, column=1, pady=5)
+eService.grid(row=0, column=1, pady=5, padx=5)
+eEmail.grid(row=1, column=1, pady=5, padx=5)
+eUsername.grid(row=2, column=1, pady=5, padx=5)
+ePassword.grid(row=3, column=1, pady=5, padx=5)
 
-enterButton = Button(master, text="Εισαγωγή Στοιχείων", command=master.quit)
+enterButton = Button(master, text="Εισαγωγή Στοιχείων", command=insertService)
 enterButton.grid(row=4, column=0, columnspan=2, sticky="we", padx=5, pady=5)
 
 
