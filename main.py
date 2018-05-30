d2ed3e1838faca1df242e53e9544fdb7f0bffdc8
Vagelis Manavis <manavis-ira@hotmail.com>
2018-05-31 00:10:27 +0300

    Clean code

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ import connectDB
 connection = connectDB.connect()
 
 try:
-    #with connection.cursor() as cursor:
-    #    sql = "INSERT INTO `user` (`username`, `password`, `email`) VALUES (%s, %s, %s)"
-    #    cursor.execute(sql, ('evanmana', 'asdfasdf', 'vagelis.manavis@gmail.com'))
-
-    #connection.commit()
-
     with connection.cursor() as cursor:
         sql = "SELECT * FROM `user`"
         cursor.execute(sql)
