4d59a148a56bf81455d5fafbb77cbe8f1c4bf9a8
Vagelis Manavis <manavis-ira@hotmail.com>
2018-12-05 22:56:22 +0200

    Add SQLite3 and remove mysql database Add <Escape> binding for exiting app.

--- mainForm.py
+++ mainForm.py
@@ -1,32 +1,28 @@
 from tkinter import *
-import connectDB
 import addNewServiceForm
+import sqlite3
+conn = sqlite3.connect('cerberus.db')
 
-def getServices():
-   print("Connecting to MySQL...")
-   connection = connectDB.connect()
-   print("Connected to MySQL...")
-   try:
-     with connection.cursor() as cursor:
-        sql = "SELECT * FROM `account`"
-        cursor.execute(sql)
-        result = cursor.fetchone()
-        while result is not None:
-            print(result)
-            result = cursor.fetchone()
-   finally:
-    connection.close()
+
+def exitApp(event):
+    master.destroy()
+
+def secretKeys(event):
+    kp = (event.char)
+    kp.replace('s', '')
+    print("Pressed:  "+kp)
 
 
 master = Tk()
-master.title('Εισαγωγή Νέας Υπηρεσίας')
+master.title('Cerberus Beta')
+master.geometry("860x400")
 master.resizable(0, 0)
 
 menubar = Menu(master)
 filemenu = Menu(menubar, tearoff=0)
 
 menubar.add_cascade(label="Cerberus", menu=filemenu)
-filemenu.add_command(label="Εισαγωγή Υπηρεσίας", command=addNewServiceForm)
+filemenu.add_command(label="Εισαγωγή Υπηρεσίας", command=addNewServiceForm.main)
 filemenu.add_command(label="Έξοδος", command=master.quit)
 
 settingsMenu = Menu(menubar, tearoff=0)
@@ -38,5 +34,6 @@ menubar.add_cascade(label="Σχετικά", menu=aboutMenu)
 master.config(menu=menubar)
 
 
-
+master.bind("<Escape>", exitApp)
+master.bind("<Key>", secretKeys)
 mainloop( )
