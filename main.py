a6d53d076d34078e6b6e2d340bfa307f34a90d93
Evangelos Manavis <manavis-ira@hotmail.com>
2020-04-09 21:52:44 +0300

    Change main.py file. From now and on this file is the Main.

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-import mainForm
+import platform
+import sqlite3
+import os
 
-print("Hello Cerberus :)")
+
+if __name__ == '__main__':
+    print(platform.system())
+    if not os.path.isfile('cerberus.db'):
+        print("Creating database...")
+        import createDatabase
+        createDatabase.main()
+
+    try:
+        conn = sqlite3.connect('cerberus.db')
+    except sqlite3.Error as e:
+        print(e)
+
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT primaryEmail, masterToken, salt FROM cerberusParameters")
+    row = cur.fetchone()
+    cur.close()
+
+    if row is None:
+        import initApp
+    else:
+        import mainForm
